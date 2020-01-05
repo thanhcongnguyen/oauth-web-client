903f30cabae695dd89c526d714338a054e3a0e65
thanhcongnguyen <thanhcong150593@gmail.com>
2020-01-05 16:37:45 +0700

    add ceate post

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,8 +16,39 @@ class Home extends Component {
                  <Helmet>
                     <title>Petplus.vn - Trang chủ</title>
                 </Helmet>
-                <div>
-                    trang chủ
+                <div id="home">
+                    <div className="create-post">
+                            <div className="input"> 
+                                <input
+                                    placeholder="Bạn đang nghĩ gì ?" 
+                                />
+                            </div>
+                            <div className="btn">
+                                <button>Đăng bài viết</button>
+                            </div>
+                            
+                    </div>
+                    <div className="post">
+                            <div className="content">nội dung</div>
+                            <div className="row mt-10 mb-10">
+                                <button className="col-11" style={{textAlign: 'right'}}><span className="share"><i className="fa fa-share-alt" aria-hidden="true"/></span></button>
+                                <button className="col-1 delete" style={{textAlign: 'left'}}><span className="delete"><i className="fa fa-trash" aria-hidden="true"/></span></button>
+                            </div>
+                    </div>
+                    <div className="post">
+                            <div className="content">nội dung</div>
+                            <div className="row mt-10 mb-10">
+                                <button className="col-11" style={{textAlign: 'right'}}><span className="share"><i className="fa fa-share-alt" aria-hidden="true"/></span></button>
+                                <button className="col-1 delete" style={{textAlign: 'left'}}><span className="delete"><i className="fa fa-trash" aria-hidden="true"/></span></button>
+                            </div>
+                    </div>
+                    <div className="post">
+                            <div className="content">nội dung</div>
+                            <div className="row mt-10 mb-10">
+                                <button className="col-11" style={{textAlign: 'right'}}><span className="share"><i className="fa fa-share-alt" aria-hidden="true"/></span></button>
+                                <button className="col-1 delete" style={{textAlign: 'left'}}><span className="delete"><i className="fa fa-trash" aria-hidden="true"/></span></button>
+                            </div>
+                    </div>
                 </div>
             </React.Fragment>
         );
