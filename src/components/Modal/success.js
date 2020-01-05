7b03273bfec55a413945da22b30a550591bd458e
thanhcongnguyen <thanhcong150593@gmail.com>
2020-01-06 02:36:30 +0700

    add success, done

--- src/components/Modal/success.js
+++ src/components/Modal/success.js
@@ -19,7 +19,7 @@ export default class ModalSuccess extends React.Component{
             <Modal show={this.props.show} onHide={this.handleClose}>
                 <Modal.Body>
                     <div className="content-success">
-                        Thao tác thành Công
+                        Tạo bài viết thành Công
                     </div>
                     <div className="wrap-btn-success">
                         
